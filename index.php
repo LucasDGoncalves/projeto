f0501bdb476039159b653039926d3abbcbc782ce
Thiago Pinto <thspinto@gmail.com>
2013-09-07 23:20:19 -0300

    Criado view e campo de busca de friends com autocomplete

--- index.php
+++ index.php
@@ -1,8 +1,86 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8" />
+<title>jQuery UI Tabs - Default functionality</title>
+<link rel="stylesheet"
+	href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+<link rel="stylesheet" href="/resources/demos/style.css" />
+<script>
+  $(function() {
+    $( "#tabs" ).tabs();
+  });
+  </script>
+<script>
+  $(function() {
+    var availableTags = [
 <?php
+require 'querys.php';
+$q = new querys();
+$users = $q->getAllUsers();
+while ( $row = mysql_fetch_array( $users ) ) {
+	echo "'{$row['nome']}'";
+	if ( mysql_num_rows( $users ) > 0)
+		echo ",";
+}
 
-require "querys.php";
+?>
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+  });
+  </script>
+</head>
 
-// importa dados da web
-$querys = new querys();
-echo $querys->addArtist('http://en.wikipedia.org/wiki/Supertramp');
-?> 
+<body>
+
+	<div id="tabs">
+		<ul>
+			<li><a href="#tabs-1">Home</a></li>
+			<li><a href="#tabs-2">Friends</a></li>
+			<li><a href="#tabs-3">Musics</a></li>
+		</ul>
+		<div id="tabs-1">
+			<p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a,
+				risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris.
+				Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem.
+				Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo.
+				Vivamus sed magna quis ligula eleifend adipiscing. Duis orci.
+				Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam
+				molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut
+				dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique
+				tempus lectus.</p>
+		</div>
+		<div id="tabs-2">
+			<div class="ui-widget">
+				<label for="tags">Pessoas: </label> <input id="tags" />
+			</div>
+		</div>
+		<div id="tabs-3">
+			<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti.
+				Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat,
+				eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent
+				taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+				himenaeos. Fusce sodales. Quisque eu urna vel enim commodo
+				pellentesque. Praesent eu risus hendrerit ligula tempus pretium.
+				Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+			<p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at,
+				semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra
+				justo vitae neque. Praesent blandit adipiscing velit. Suspendisse
+				potenti. Donec mattis, pede vel pharetra blandit, magna ligula
+				faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque.
+				Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi
+				lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean
+				vehicula velit eu tellus interdum rutrum. Maecenas commodo.
+				Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus
+				hendrerit hendrerit.</p>
+		</div>
+	</div>
+
+
+</body>
+</html>
