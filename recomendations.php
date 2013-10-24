d444b741e42c11823e21aee4830f9b100c592745
Thiago Pinto <thspinto@gmail.com>
2013-10-24 14:05:55 -0200

    querys recomendações funcionando

--- recomendations.php
+++ recomendations.php
@@ -9,29 +9,31 @@ class recomendations {
 		// cria cconecta ao banco
 		$this->con->conecta ();
 		
-		$query = "select artista_genero.id_genero as genero from pessoa 
+		$query = "select artista_genero.id_genero as genre from pessoa 
 				join curtida on pessoa.login = curtida.login
 				join artista on curtida.id_artista = artista.id
 				join artista_genero on artista.id = artista_genero.id_artista
 				join genero on artista_genero.id_genero = genero.id
 				where pessoa.login like '{$login}'
-				group by artista_genero.id_genero
+				group by genero.id
 				order by count(*) desc
 				limit 5";
 		$res = mysql_query ( $query ) or die ( mysql_error () );
 		while ( $rs = mysql_fetch_assoc ( $res ) ) {
-			$result [] = $rs ['genero'];
+			$result1 [] = $rs ['genre'];
 		}
 		
+		$result1 = implode(", " , $result1);
 		$query = "select artista.nome_artistico as nome, count(*) as count from artista_genero 
 				join artista on  artista_genero.id_artista = artista.id
 				join curtida on curtida.id_artista = artista.id
-				where artista_genero.id_genero in {$result}
+				where artista_genero.id_genero in ({$result1})
 				group by curtida.id_artista
-				order by count(*) desc";
+				order by count desc";
 		$res = mysql_query ( $query ) or die ( mysql_error () );
+		
 		while ( $rs = mysql_fetch_assoc ( $res ) ) {
-			$result [] = $rs [];
+			$result [] = $rs;
 		}
 		
 		$this->con->fecha ();
@@ -42,14 +44,15 @@ class recomendations {
 		// cria cconecta ao banco
 		$this->con->conecta ();
 		
-		$query = "select *, count(*) from curtida
+		$query = "select similar.descricao as nome, count(*) as count from curtida
 				join artista_similar on curtida.id_artista = artista_similar.id_artista
-				where curtida.login like '{login}'
+				join similar on artista_similar.id_similar = similar.id
+				where curtida.login like '{$login}'
 				group by artista_similar.id_similar
-				order by count(*) desc";
+				order by count desc";
 		$res = mysql_query ( $query ) or die ( mysql_error () );
 		while ( $rs = mysql_fetch_assoc ( $res ) ) {
-			$result [] = $rs [];
+			$result [] = $rs;
 		}
 		
 		$this->con->fecha ();
@@ -60,14 +63,15 @@ class recomendations {
 		// cria cconecta ao banco
 		$this->con->conecta ();
 	
-		$query = "select id_artista, (sum(curtida.nota)/5) as peso, count(*) from conhecimento
+		$query = "select artista.nome_artistico, (sum(curtida.nota)/5) as peso, count(*) from conhecimento
 				join curtida on curtida.login = conhecimento.conhecido
-				where conhecimento.conhecedor = 'lucasgoncalves'
+				join artista on curtida.id_artista = artista.id
+				where conhecimento.conhecedor = '{$login}'
 				group by curtida.id_artista
 				order by peso desc";
 		$res = mysql_query ( $query ) or die ( mysql_error () );
 		while ( $rs = mysql_fetch_assoc ( $res ) ) {
-			$result [] = $rs [];
+			$result [] = $rs;
 		}
 	
 		$this->con->fecha ();
@@ -75,6 +79,6 @@ class recomendations {
 	}
 }
 $rec = new recomendations ();
-$rec->generoPop ( 'lucasgoncalves' );
+$rec->amigos ( 'lucasgoncalves' );
 
 ?>
